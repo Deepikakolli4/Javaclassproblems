a462cbb2164c48d44918e1aac2b0c022520307c2
Deepikakolli4 <deepikakolli2004@gmail.com>
2024-07-03 21:48:02 +0530

    added new
    Signed-off-by: Deepikakolli4 <deepikakolli2004@gmail.com>

--- src/ReadingFiles.java
+++ src/ReadingFiles.java
@@ -2,6 +2,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 
 public class ReadingFiles {
@@ -20,5 +21,20 @@ public class ReadingFiles {
         System.out.println("number of lines in the file are : " + lines.size());
         System.out.println("number of words in the file are : " + wordCount);
         System.out.println("number of characters in the file are : " + charCount);
+        sortContents(path,file);
     }
-}
+
+    public static void sortContents(String path, String file) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(path, file));
+        System.out.println("Before sorting:");
+        for (String line : lines) {
+            System.out.println(line);
+        }
+
+        Collections.sort(lines);
+        System.out.println("After sorting:");
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+    }
