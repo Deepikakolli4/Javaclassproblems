3b378d7ee1a5a2a0bf76a91c63617a818ba2facb
Deepikakolli4 <deepikakolli2004@gmail.com>
2024-07-14 19:03:24 +0530

    updated
    Signed-off-by: Deepikakolli4 <deepikakolli2004@gmail.com>

--- src/Occurence.java
+++ src/Occurence.java
@@ -1,14 +1,16 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Occurence {
     public static void main(String[] args) throws IOException {
         String inputPath = "C:\\Users\\kolli\\OneDrive\\Documents\\sample.txt";
         List<String> lines = Files.readAllLines(Paths.get(inputPath));
-        boolean flag = false;
+        //   boolean flag = false;
         HashMap<String, Integer> occurrence = new HashMap<>();
         for (String line : lines) {
             String[] words = line.split("\\s+");
@@ -16,15 +18,18 @@ public class Occurence {
                 occurrence.put(reqwords.toLowerCase(), occurrence.getOrDefault(reqwords.toLowerCase(), 0) + 1);
             }
         }
-        System.out.println("Words with 10 occurrences:");
-        for (String word : occurrence.keySet()) {
-            if (occurrence.get(word) == 10) {
-                flag = true;
-                System.out.println("Word: " + word + ", Occurrences: " + occurrence.get(word));
-            }
-        }
-        if (flag == false) {
-            System.out.println("No word is Occuring 10 times");
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(occurrence.entrySet());
+        list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println("Word: " + entry.getKey() + ", Occurrences: " + entry.getValue());
         }
     }
 }
+//            if (occurrence.get(word) == 10) {
+//                flag = true;
+//                System.out.println("Word: " + word + ", Occurrences: " + occurrence.get(word));
+//            }
+//        }
+//        if (flag == false) {
+//            System.out.println("No word is Occuring 10 times");
+//        }
