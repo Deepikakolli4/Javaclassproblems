e3b84812acb2a81f32d61213c70289bcd0e74736
Deepikakolli4 <deepikakolli2004@gmail.com>
2024-08-06 16:26:05 +0530

    update
    Signed-off-by: Deepikakolli4 <deepikakolli2004@gmail.com>

--- src/LargestSmallest.java
+++ src/LargestSmallest.java
@@ -8,18 +8,16 @@ public class LargestSmallest {
         String number = Integer.toString(input);
         int smallest = Integer.MAX_VALUE;
         int largest = Integer.MIN_VALUE;
-        int[] digits = new int[number.length()];
         for (int i = 0; i < number.length(); i++) {
-            digits[i] = number.charAt(i) - '0';
-        }
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] < smallest) {
-                smallest = digits[i];
+            int digit = number.charAt(i) - '0';
+            if (digit < smallest) {
+                smallest = digit;
             }
         }
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] > largest) {
-                largest = digits[i];
+        for (int i = 0; i < number.length(); i++) {
+            int digit = number.charAt(i) - '0';
+            if (digit > largest) {
+                largest = digit;
             }
         }
         System.out.println(smallest + "\t" + largest);
